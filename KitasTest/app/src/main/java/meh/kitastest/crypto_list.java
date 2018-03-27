84ebe5c92d5a08b0adda4827a6bbc1afc7ae70fb
AwFro <aiwapetrikas@gmail.com>
2018-03-27 17:00:43 +0300

    pavyko JSON gaut
    yeeeeeeeeeeee

--- KitasTest/app/src/main/java/meh/kitastest/crypto_list.java
+++ KitasTest/app/src/main/java/meh/kitastest/crypto_list.java
@@ -2,7 +2,12 @@ package meh.kitastest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class crypto_list extends AppCompatActivity {
 
@@ -12,22 +17,36 @@ public class crypto_list extends AppCompatActivity {
     String[] valiutos = {"Bitcoin", "Ethereum", "Litecoin", "Ripple"};
     Integer[] imgid={R.drawable.btc, R.drawable.eth, R.drawable.litecoin, R.drawable.ripple};
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crypto_list);
 
 
+            // darbas su data_download klase:
+        data_download dl = new data_download(new data_download.AsyncCallback() {
+            @Override
+            public void gavauData(String result) {
+                toliau(result);
+            }
+        });
+        dl.webSite = "https://api.coinmarketcap.com/v1/ticker/";
+        dl.execute();
+    }
+    private void toliau(String result){
 
+        try {
+            JSONArray textas = new JSONArray(result);
+            Log.d("myTag", textas.getJSONObject(0).getString("symbol").toString());
 
-        list = (ListView) findViewById(R.id.listView);
-        list_adapter_crypto custom = new list_adapter_crypto(this, trumpiniai, valiutos, imgid);
-        list.setAdapter(custom);
-
+            list = (ListView) findViewById(R.id.listView);
+           //list_adapter_crypto custom = new list_adapter_crypto(this, textas, imgid);
+            list_adapter_crypto custom = new list_adapter_crypto(this, trumpiniai, valiutos, imgid);
+            list.setAdapter(custom);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
-
-
-
-
 }
--- KitasTest/app/src/main/java/meh/kitastest/list_adapter_crypto.java
+++ KitasTest/app/src/main/java/meh/kitastest/list_adapter_crypto.java
@@ -1,16 +1,94 @@
 package meh.kitastest;
 
-import android.app.Activity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
+        import android.app.Activity;
+        import android.util.Log;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.BaseAdapter;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+
+        import org.json.JSONArray;
+        import org.json.JSONException;
+        import org.json.JSONObject;
 
 /**
  * Created by Aivaras on 2018-03-26.
  */
+/*
+public class list_adapter_crypto extends BaseAdapter
+{
+    private String[] trumpinys;
+    private String[] pavadinimas;
+    private Integer[] imgid;
+    private  JSONArray textas;
+
+    private Activity context;
+    private LayoutInflater inflater;
+    public list_adapter_crypto(Activity context, JSONArray textas, Integer[] imgid)
+    {
+        this.textas = textas;
+        this.context = context;
+       // this.trumpinys= name;
+       // this.pavadinimas = mobile_no;
+        this.imgid = imgid;
+        this.inflater = context.getLayoutInflater();
+    }
+
+    @Override
+    public int getCount() {
+        Log.d("buuuum", textas.length()+"");
+        return textas.length();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return textas.optJSONObject(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        JSONObject jsonObject = textas.optJSONObject(position);
+        return jsonObject.optLong("id");
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        ViewHolder holder;
+        if(convertView==null){
+            holder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.listo_body, null);
+            holder.trump = (TextView) convertView.findViewById(R.id.trumpinys);
+            holder.pavadin = (TextView) convertView.findViewById(R.id.pavadinimas);
+            holder.img = (ImageView) convertView.findViewById(R.id.logo);
+
+            convertView.setTag(holder);
+        }else{
+            holder = (ViewHolder)convertView.getTag();
+        }
+
+        try {
+            holder.trump.setText(textas.getJSONObject(position).getString("symbol").toString());
+            holder.pavadin.setText(textas.getJSONObject(position).getString("name").toString());
+            holder.img.setImageResource(imgid[position]);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return convertView;
+    }
+    class ViewHolder{
+        TextView trump, pavadin;
+        ImageView img; // atskiri
+
+    }
+}
+*/
+
+//jei ka:
 
 public class list_adapter_crypto extends BaseAdapter
 {
