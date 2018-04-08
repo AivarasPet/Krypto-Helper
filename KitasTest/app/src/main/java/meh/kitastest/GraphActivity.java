ea100645c325d3144c09719eaaeaeafbe2ca302d
AwFro <aiwapetrikas@gmail.com>
2018-04-08 11:53:34 +0300

    graphActivity...
    atskira klase grafikams brezt, idetas spinneris, radiobuttonai ir pns...

--- KitasTest/app/src/main/java/meh/kitastest/GraphActivity.java
+++ KitasTest/app/src/main/java/meh/kitastest/GraphActivity.java
@@ -14,7 +14,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,9 +35,10 @@ import org.json.JSONObject;
 import java.util.Date;
 
 
-public class GraphActivity extends AppCompatActivity {
+public class GraphActivity extends AppCompatActivity{
 
     TextView testas;
+    GraphView graph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,97 +46,36 @@ public class GraphActivity extends AppCompatActivity {
         setContentView(R.layout.activity_graph);
 
         testas = (TextView) findViewById(R.id.TestInGraph);
+        graph = (GraphView) findViewById(R.id.graph);
 
-        data_download dl = new data_download(new data_download.AsyncCallback() {
-            @Override
-            public void gavauData(String result) {
-                toliau(result);
-            }
-        });
-        dl.webSite = "https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=30&aggregate=1&e=CCCAGG";
-        dl.execute();
+        Spinner spinner = (Spinner) findViewById(R.id.dropDown);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.cryptoNames, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+// Apply the adapter to the spinner
+         spinner.setAdapter(adapter);
+        //spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           // @Override
+           // public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+          //  }
+        //});
+
+
+        graph_adapter naujas = new graph_adapter();
+        naujas.graph = graph;
+        naujas.txt = testas;
+        naujas.context = getApplicationContext();
+        naujas.run("https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=30&aggregate=1&e=CCCAGG");
+
+
 
     }
 
 
 
-    private void toliau(String result) {
-        try {
-            JSONObject obj = new JSONObject(result);
-            JSONArray textas = obj.getJSONArray("Data");
-            //Log.d("myTag", textas.getJSONObject(0).getString("symbol").toString());
-            //Log.d("Log.d", textas.length()+"");
-            String laikas;
-
-            float b;
-            int l;
-            Date date;
-            final String[] diena = new String[32];
 
 
-            GraphView graph = (GraphView) findViewById(R.id.graph);
-            LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
-            for(int x=0; x<30; x++) {
-                laikas = textas.getJSONObject(x).getString("time").toString();
-                l = Integer.parseInt(laikas);
-                date = new java.util.Date(l*1000L);
-                diena[x] = (String) android.text.format.DateFormat.format("MM/dd",  date);
-                b = Float.parseFloat(textas.getJSONObject(x).getString("close").toString());
-                series.appendData(new DataPoint(x, b), true, 30);
-            }
-
-            graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
-                @Override
-                public String formatLabel(double value, boolean isValueX) {
-                    if (isValueX) {
-                        // show  x values
-                        //return diena[Integer.parseInt(""+value)];
-                        int x = (int ) value;
-                        Log.d("CRash", x+" "+value);
-                        return diena[x];
-                    } else {
-                        // show  y values
-                        return super.formatLabel(value, isValueX)+"$";
-                    }
-                }
-            });
-            //testas.setText(a);
-
-            series.setOnDataPointTapListener(new OnDataPointTapListener() {
-                @Override
-                public void onTap(Series series, DataPointInterface dataPoint) {
-                    testas.setText("$"+dataPoint.getY());
-                }
-            });
-
-            series.setColor(Color.parseColor("#f7931a"));
-            series.setThickness(10);
-            series.setDrawBackground(true);
-            series.setBackgroundColor(Color.parseColor("#fcd19c"));
-
-
-            //xzn kazka pagrazina???
-            Paint paint = new Paint();
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeWidth(5);
-            paint.setPathEffect(new DashPathEffect(new float[]{18, 5}, 0));
-            series.setCustomPaint(paint);
-
-
-            graph.setTitle("BTC");
-            graph.getViewport().setXAxisBoundsManual(true);
-            graph.getViewport().setMinX(20);
-            graph.getViewport().setMaxX(30);
-
-            //graph.getViewport().setYAxisBoundsManual(true);
-            //graph.getViewport().setMinY(textas.);
-            //graph.getViewport().setMaxY(12000);
-            graph.getViewport().setScalable(true);
-            graph.addSeries(series);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-            Toast.makeText(getApplicationContext(),"Check your internet connection!",Toast.LENGTH_LONG).show();
-        }
-    }
 }
---
+++ KitasTest/app/src/main/java/meh/kitastest/graph_adapter.java
@@ -0,0 +1,125 @@
+package meh.kitastest;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.Series;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+
+public class graph_adapter {
+
+
+
+
+
+    TextView txt;
+    public GraphView graph;
+    Context context;
+
+    public void run(String url) {
+        data_download dl = new data_download(new data_download.AsyncCallback() {
+            @Override
+            public void gavauData(String result) {
+                toliau(result);
+                //Toast.makeText(context,"Check connection!",Toast.LENGTH_LONG).show();
+            }
+        });
+        dl.webSite = url;
+        dl.execute();
+    }
+
+    private void toliau(String result) {
+        try {
+            JSONObject obj = new JSONObject(result);
+            JSONArray textas = obj.getJSONArray("Data");
+            //Log.d("myTag", textas.getJSONObject(0).getString("symbol").toString());
+            //Log.d("Log.d", textas.length()+"");
+            String laikas;
+
+            float b;
+            int l;
+            Date date;
+            final String[] diena = new String[32];
+
+
+            LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
+            for(int x=0; x<30; x++) {
+                laikas = textas.getJSONObject(x).getString("time").toString();
+                l = Integer.parseInt(laikas);
+                date = new java.util.Date(l*1000L);
+                diena[x] = (String) android.text.format.DateFormat.format("MM/dd",  date);
+                b = Float.parseFloat(textas.getJSONObject(x).getString("close").toString());
+                series.appendData(new DataPoint(x, b), true, 30);
+            }
+
+            graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+                @Override
+                public String formatLabel(double value, boolean isValueX) {
+                    if (isValueX) {
+                        // show  x values
+                        //return diena[Integer.parseInt(""+value)];
+                        int x = (int ) value;
+                        Log.d("CRash", x+" "+value);
+                        return diena[x];
+                    } else {
+                        // show  y values
+                        return super.formatLabel(value, isValueX)+"$";
+                    }
+                }
+            });
+            //testas.setText(a);
+
+            series.setOnDataPointTapListener(new OnDataPointTapListener() {
+                @Override
+                public void onTap(Series series, DataPointInterface dataPoint) {
+                    txt.setText("$"+dataPoint.getY());
+                }
+            });
+
+            series.setColor(Color.parseColor("#f7931a"));
+            series.setThickness(10);
+            series.setDrawBackground(true);
+            series.setBackgroundColor(Color.parseColor("#fcd19c"));
+
+
+            //xzn kazka pagrazina???
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(5);
+            paint.setPathEffect(new DashPathEffect(new float[]{18, 5}, 0));
+            series.setCustomPaint(paint);
+
+
+            graph.setTitle("BTC");
+            graph.getViewport().setXAxisBoundsManual(true);
+            graph.getViewport().setMinX(20);
+            graph.getViewport().setMaxX(30);
+
+            //graph.getViewport().setYAxisBoundsManual(true);
+            //graph.getViewport().setMinY(textas.);
+            //graph.getViewport().setMaxY(12000);
+            graph.getViewport().setScalable(true);
+            graph.addSeries(series);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(context,"Check your internet connection!",Toast.LENGTH_LONG).show();
+        }
+    }
+}
